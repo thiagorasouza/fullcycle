16db6e2e46bb47108aad36c1038277f2d52475be
Thiago Souza <thiagorasouza@gmail.com>
2023-10-26 19:02:11 -0500

    startover nginx-node-mysql challenge

--- docker/reverse-proxy/node/index.js
+++ docker/reverse-proxy/node/index.js
@@ -1,36 +1,41 @@
 const http = require("http");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const faker = require("@faker-js/faker").faker;
 
 const config = {
   host: "mysql",
   user: "root",
   password: "root",
-  database: "db"
-}
+  database: "node-db",
+};
 
 const server = http.createServer((req, res) => {
   try {
-    const fakeName = faker.name.findName();
+    const fakeName = faker.person.fullName();
 
-    const connection = mysql.createConnection(config)
-    connection.query(`CREATE TABLE if not exists people (id int not null auto_increment, name varchar(255) not null, primary key(id))`);
-    connection.query(`INSERT INTO people(name) values('${fakeName}')`);
-    connection.query(`SELECT * FROM people`, function (error, results, fields) {
-      if (error) throw error;
+    const connection = mysql.createConnection(config);
+    connection.query(
+      `CREATE TABLE if not exists people (id int not null auto_increment, name varchar(255) not null, primary key(id));`
+    );
+    connection.query(`INSERT INTO people(name) values('${fakeName}');`);
+    connection.query(
+      `SELECT * FROM people;`,
+      function (error, results, fields) {
+        if (error) throw error;
 
-      res.statusCode = 200;
-      return res.end(`
+        res.statusCode = 200;
+        return res.end(`
         <h1>Full Cycle Rocks!</h1>
         <ul>
-          ${results.map(person => (`<li>${person.id}: ${person.name}</li>`)).join("")}
+          ${results
+            .map((person) => `<li>${person.id}: ${person.name}</li>`)
+            .join("")}
         </ul>
-      `)
-    });
+      `);
+      }
+    );
     connection.end();
-
-    
-  } catch {
+  } catch (error) {
     console.log(error);
     res.statusCode = 500;
     return res.end("Server error");
@@ -39,5 +44,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, () => {
   console.log("Server is listening on 3000");
-})
-
+});
